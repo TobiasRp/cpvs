6c890819df3ea3bf7a82400b9c8ba1a7528a9992
Tobias Rapp <tobias.rapp@student.kit.edu>
2014-12-27 17:00:48 +0100

    Add OpenCL to buildsystem and a thin wrapper for the cpp bindings

---
+++ src/ClProgram.cpp
@@ -0,0 +1,86 @@
+#include "ClProgram.h"
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <algorithm>
+
+using namespace std;
+
+static cl::Context context;
+static vector<cl::Device> devices;
+static vector<cl::CommandQueue> commandQueues;
+
+const char* getErrorString(cl_int err) {
+	switch(err) {
+	case CL_INVALID_VALUE:
+		return "CL_INVALID_VALUE";
+	case CL_OUT_OF_HOST_MEMORY:
+		return "CL_OUT_OF_HOST_MEMORY";
+	case CL_PLATFORM_NOT_FOUND_KHR:
+		return "CL_PLATFORM_NOT_FOUND_KHR";
+	}
+	return "";
+}
+
+void checkCLErrors(cl_int err, const char* name) {
+	if (err != CL_SUCCESS) {
+		std::cerr << "Error: " << name << " (" << getErrorString(err) << ")" << std::endl;
+		std::terminate();
+	}
+}
+
+void initializeCL() {
+	vector<cl::Platform> platformList;
+	cl::Platform::get(&platformList);
+
+	for (const auto& pl : platformList) {
+		string vendor;
+		pl.getInfo((cl_platform_info)CL_PLATFORM_VENDOR, &vendor);
+		cout << "Platform is by: " << vendor << "\n";
+	}
+	cl::Platform defaultPlatform = platformList[0];
+
+	defaultPlatform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+	for (const auto& dev : devices) {
+		string version;
+		dev.getInfo((cl_platform_info)CL_DEVICE_VERSION, &version);
+		cout << "OpenCL device version is: " << version << "\n";
+	}
+
+	cl_context_properties cps[] = {
+		CL_CONTEXT_PLATFORM,
+		(cl_context_properties)(platformList[0])(),
+		0
+	};
+	context = cl::Context(CL_DEVICE_TYPE_GPU, cps);
+
+	for_each(devices.begin(), devices.end(), [ctx = context](auto dev)
+			{ commandQueues.push_back(cl::CommandQueue(ctx, dev)); });
+}
+
+ClProgram::ClProgram(string sourceFile) {
+	auto sources = readSource(sourceFile);
+	m_program = cl::Program(context, sources);
+	m_program.build(devices);
+}
+
+cl::Program::Sources ClProgram::readSource(const std::string& sourceFile) {
+	ifstream file(sourceFile);
+	string code(istreambuf_iterator<char>(file), (istreambuf_iterator<char>()));
+	return cl::Program::Sources(1, make_pair(code.c_str(), code.length()+1));
+}
+
+cl::Context ClProgram::getContext() const {
+	return context;
+}
+
+cl::CommandQueue ClProgram::getCommandQueue(unsigned deviceNr) const {
+	assert(deviceNr < commandQueues.size());
+	return commandQueues[deviceNr];
+}
