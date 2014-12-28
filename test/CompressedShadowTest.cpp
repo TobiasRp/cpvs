303542c2cc95afa54b3822d0a77083c475e8c69c
Tobias Rapp <tobias.rapp@student.kit.edu>
2014-12-28 19:13:22 +0100

    Test case for GLSL traversal (a bit complicated...)

--- test/CompressedShadowTest.cpp
+++ test/CompressedShadowTest.cpp
@@ -79,6 +79,9 @@ TEST_F(CompressedShadowTest, testTraverse8x8) {
 	vis = csPtr->traverse(vec3(1, 1, 0));
 	ASSERT_EQ(CompressedShadow::SHADOW, vis);
 
+	vis = csPtr->traverse(vec3(0, 0, 0));
+	ASSERT_EQ(CompressedShadow::SHADOW, vis);
+
 	vis = csPtr->traverse(vec3(0.5, 0.5, 0.5));
 	ASSERT_EQ(CompressedShadow::SHADOW, vis);
 
@@ -119,4 +122,74 @@ TEST_F(CompressedShadowTest, testTraverse16x16) {
 
 	vis = csPtr->traverse(vec3(-1, 1, -1));
 	ASSERT_EQ(CompressedShadow::SHADOW, vis);
+}
+
+TEST_F(CompressedShadowTest, testCompute8) {
+	MinMaxHierarchy mm(img8);
+	auto csPtr = CompressedShadow::create(mm);
+
+	// Some test positions in the range [-1, 1]^4. 
+	// These values give a nice visibility pattern (which is easy to test), see below.
+	ImageF positions(8, 8, 4);
+	positions.setAll(vector<float>{
+		// 1st row
+		-1, 1, -.5, 1,  -.65, 1, -.5, 1,  -.4, 1, -.5, 1,  -.15, 1, 0, 1,
+		.1, 1,   0, 1,   .40, 1,   0, 1,  .65, 1,   0, 1,  1, 1, -.4, 1,
+
+		// 2nd row 
+		-1, .65, 0, 1,  -.65, .65, 0, 1,  -.4, .65, 0, 1,  -.15, .65, 0, 1,
+		.1, .65, 0, 1,   .40, .65, 0, 1,  .65, .65, 0, 1,  1, .65, -.4, 1,
+
+		// 3rd ...
+		-1, .4, 0, 1,  -.65, .4, 0, 1,  -.4, .4, 0, 1,  -.15, .4, 0, 1,
+		.1, .4, 0, 1,   .40, .4, 0, 1,  .65, .4, 0, 1,  1, .4, -.4, 1,
+
+		// below are correct
+
+		-1, .1, 0, 1,  -.65, .1, 0, 1,  -.4, .1, 0, 1,  -.15, .1, 0, 1,
+		.1, .1, 0, 1,   .40, .1, 0, 1,  .65, .1, 0, 1,  1, .1, -.4, 1,
+
+		-1, -.15, 0, 1,  -.65, -.15, 0, 1,  -.4, -.15, 0, 1,  -.15, -.15, 0, 1,
+		.1, -.15, 0, 1,   .40, -.15, 0, 1,  .65, -.15, 0, 1,  1, -.15, -.4, 1,
+
+		-1, -.4, 0, 1,  -.65, -.4, 0, 1,  -.4, -.4, 0, 1,  -.15, -.4, 0, 1,
+		.1, -.4, 0, 1,   .40, -.4, 0, 1,  .65, -.4, 0, 1,  1, -.4, -.4, 1,
+
+		-1, -.65, 0, 1,  -.65, -.65, 0, 1,  -.4, -.65, 0, 1,  -.15, -.65, 0, 1,
+		.1, -.65, 0, 1,   .40, -.65, 0, 1,  .65, -.65, 0, 1,   1, -.65, -.4, 1,
+
+		-1, -1, 0, 1,  -.65, -1, 0, 1,  -.4, -1, 0, 1,  -.15, -1, 0, 1,
+		.1, -1, 0, 1,   .40, -1, 0, 1,  .65, -1, 0, 1,  1, -1, 0, 1});
+
+	Texture2D positionsTex(positions);
+
+	mat4 lightViewProj(1); //identity matrix 4x4
+
+	Texture2D result(8, 8, GL_R8, GL_RED, GL_FLOAT);
+
+	csPtr->compute(&positionsTex, lightViewProj, &result);
+	ASSERT_EQ(GL_NO_ERROR, glGetError());
+
+	result.bindAt(0);
+	ImageF resultImg(8, 8, 1);
+	glGetTexImage(GL_TEXTURE_2D, 0, result.getFormat(), result.getType(), resultImg.data());
+
+	// Resulting visibilites are now in resultImg
+	
+	for(size_t y = 0; y < 8; ++y) {
+		for (size_t x = 0; x < 8; ++x) {
+			// Test some visibilities (which are heaviliy influenced by the test positions above!)
+
+			int vis = resultImg.get(x, y, 0);
+
+			// right border is visible
+			if (x % 8 == 7)
+				ASSERT_EQ(1, vis);
+			// in the lower half the two leftmost columns should be visible
+			else if (y >= 4 && (x == 0 || x == 1))
+				ASSERT_EQ(1, vis);
+			else
+				ASSERT_EQ(0, vis);
+		}
+	}
 }
