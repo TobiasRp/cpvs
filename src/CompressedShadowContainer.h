420f284ab1c942ecacc5c7cb19bf3ea26bd0e979
Tobias Rapp <tobias.rapp@student.kit.edu>
2015-01-29 13:34:51 +0100

    Some documentation

--- src/CompressedShadowContainer.h
+++ src/CompressedShadowContainer.h
@@ -17,12 +17,14 @@ class Texture2D;
  */
 class CompressedShadowContainer {
 public:
+	/** Creates a container with a fixed length in one dimension of the 3D container. */
 	CompressedShadowContainer(uint length)
 		: m_length(length)
 	{
 		m_data.resize(length * length * length);
 	}
 
+	/** Creates a container with a length of 1 and initializes it with the given precomputed shadow. */
 	CompressedShadowContainer(unique_ptr<CompressedShadow> shadow)
 		: m_length(1)
 	{
@@ -49,28 +51,23 @@ public:
 	 */
 	void evaluate(const Texture2D* positionsWS, const mat4& lightViewProj, Texture2D* visibilities);
 
-	/**
-	 * Frees all dynamically allocated memory on the CPU.
-	 */
+	/** Frees all dynamically allocated memory on the CPU. */
 	inline void freeOnCPU() {
 		// Use the 'swap trick' to free all dynamic memory
 		std::vector<unique_ptr<CompressedShadow>> tmp;
 		m_data.swap(tmp);
 	}
 
-	/**
-	 * Copy all shadows to the device memory.
-	 */
+	/** Copy all shadows to the device memory. */
 	void copyToGPU();
 
-	/**
-	 * Combines copyToGPU and freeDagOnCPU.
-	 */
+	/** Combines copyToGPU and freeOnCPU, i.e. copies the data to the GPU and free's it on the CPU */
 	inline void moveToGPU() {
 		copyToGPU();
 		freeOnCPU();
 	}
 
+private:
 	void initShader();
 
 	vector<uint> createTopLevelGrid();
